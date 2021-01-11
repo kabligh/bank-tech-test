9ff612c2405c7e2222f59a241c1de9a5effa421a
kabligh <>
2021-01-11 12:27:48 +0000

    Pass test for Account starting balance being 0

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,9 @@
 class Account
 
+  def balance
+    0
+  end
+
   def deposit(amount)
   end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,9 @@ describe Account do
 
   it { is_expected.to respond_to(:deposit).with(1).argument }
 
+  it 'has a starting balance of 0' do
+      account = Account.new
+      expect(account.balance).to eq 0
+  end
+
 end
