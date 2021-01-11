99152f852722ca703384c00c8093508a7ae7f53c
kabligh <>
2021-01-11 12:39:43 +0000

    Add withdraw method that reduces account balance by amount given in its argument

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,19 +2,26 @@ require 'account'
 
 describe Account do
 
-  it { is_expected.to respond_to(:deposit).with(1).argument }
+  let(:account) { Account.new }
 
   it 'has a starting balance of 0' do
-      account = Account.new
       expect(account.balance).to eq 0
   end
 
+  it { is_expected.to respond_to(:deposit).with(1).argument }
+
   describe '#deposit' do
     it 'increases the account balance by amount in argument' do
-      account = Account.new
       account.deposit(1000)
       expect(account.balance).to eq 1000
     end
   end
 
+  describe '#withdraw' do
+    it 'reduces the account balance by amount in argument' do
+      account.deposit(1000)
+      account.withdraw(500)
+      expect(account.balance).to eq 500
+    end
+  end
 end
