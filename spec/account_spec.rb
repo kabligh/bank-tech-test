3e1391b19ea0d8f2b7f93b2938d540dbb8b7cf3f
kabligh <>
2021-01-11 15:58:16 +0000

    Add empty transaction history array to Account class upon initialisation

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,8 +4,15 @@ describe Account do
 
   let(:account) { Account.new }
 
-  it 'has a starting balance of 0' do
-    expect(account.balance).to eq 0
+  describe '#initialize' do
+    it 'has a starting balance of 0' do
+      expect(account.balance).to eq 0
+    end
+
+    it 'starts with an empty transaction history' do
+      expect(account.transaction_history.length).to eq 0
+    end
+
   end
 
   it { is_expected.to respond_to(:deposit).with(1).argument }
@@ -24,4 +31,5 @@ describe Account do
       expect(account.balance).to eq 500
     end
   end
+
 end
